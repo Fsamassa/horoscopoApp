d65576bedf5f4b32a067f8dc1bf84521c5f12902
Facundo Samassa <facundo.samassa@hotmail.com>
2023-10-29 12:49:22 -0300

    Agrego Interceptors

--- app/src/main/java/com/example/horoscopoapp/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/horoscopoapp/data/network/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.example.horoscopoapp.data.network
 
 import com.example.horoscopoapp.data.RepositorioImpl
+import com.example.horoscopoapp.data.core.interceptor.AuthInterceptor
 import com.example.horoscopoapp.domain.Repositorio
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,13 +19,27 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit():Retrofit{
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
         return Retrofit
             .Builder()
             .baseUrl("https://newastro.vercel.app/")
+            .client(okHttpClient)    // <- Interceptor
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
+    }
+
+    // Interceptor -> Me indica en el log info cuando hago accesos a la API externa
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(authInterceptor)
+            .build()
     }
 
     @Provides
