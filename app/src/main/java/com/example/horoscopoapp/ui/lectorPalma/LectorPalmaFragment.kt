07587c70e2f1ed11d12aa788602080becab02aba
Facundo Samassa <facundo.samassa@hotmail.com>
2023-10-31 17:49:27 -0300

    Subo permisos para la camara

--- app/src/main/java/com/example/horoscopoapp/ui/lectorPalma/LectorPalmaFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/lectorPalma/LectorPalmaFragment.kt
@@ -1,9 +1,14 @@
 package com.example.horoscopoapp.ui.lectorPalma
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.example.horoscopoapp.databinding.FragmentLectorPalmaBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +16,42 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class LectorPalmaFragment : Fragment() {
 
+    companion object{
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentLectorPalmaBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){isGranted ->
+        if (isGranted){
+
+        }else{
+            Toast.makeText(
+                requireContext(),
+                "Acepta permisos para poder usar el lector de manos.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()){
+
+        }else{
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
+
+        }
+
+    }
+
+    fun checkCameraPermission(): Boolean{
+        return PermissionChecker.checkSelfPermission(requireContext(), CAMERA_PERMISSION) == PermissionChecker.PERMISSION_GRANTED // android.Manifest.permission.CAMERA
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
